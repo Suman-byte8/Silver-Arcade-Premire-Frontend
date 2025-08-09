7aa7bea5e2e941ce5d631548233b2b8f55a8b268
SumanSaha <sumansahaweb.dev@gmail.com>
2025-08-09 22:44:15 +0530

    feat : change the import statement in home comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import Carousel from "../components/Home Page/Carousel/Carousel";
 import Distinctive from "../components/Home Page/Distinctive/Distinctive";
 
-import Offers from "../components/Home Page/Curated Offers/offers";
+import Offers from "../components/Home Page/Curated Offers/Offers";
 
 const Home = () => {
   return (
